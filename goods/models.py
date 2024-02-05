84949c48674c787b73eef5d11c8b72c9b592af87
erool <erool2016@yandex.ru>
2024-02-05 17:50:54 +0300

    create models.py - categories and Product,admin,setting.py,

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=150, unique=True,verbose_name='Название')
+    slug = models.SlugField(max_length=200,unique=True,blank=True,null=True,verbose_name='URL')
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'Категорию'
+        verbose_name_plural = 'Категория'
+
+class Products(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    description = models.TextField(blank=True, null=True,verbose_name='описание')
+    image = models.ImageField(upload_to='goods_images',blank=True, null=True,verbose_name='изображение')
+    price = models.DecimalField(default=0.00,max_digits=7,decimal_places=2,verbose_name='цена')
+    discount = models.DecimalField(default=0.00,max_digits=7,decimal_places=2,verbose_name='скидка %')
+    quantity = models.PositiveIntegerField(default=0,verbose_name='количество')
+    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE,verbose_name='категория')
+
+    class Meta:
+        db_table = 'product'
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
+
+
