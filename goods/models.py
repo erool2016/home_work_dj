5953f57852865a9c04b727af595a59e48e2a5a25
erool <erool2016@yandex.ru>
2024-02-05 20:32:17 +0300

    requriments,models, admin - class Admin

--- goods/models.py
+++ goods/models.py
@@ -8,7 +8,10 @@ class Categories(models.Model):
     class Meta:
         db_table = 'category'
         verbose_name = 'Категорию'
-        verbose_name_plural = 'Категория'
+        verbose_name_plural = 'Категорияэ'
+
+    def __str__(self):
+        return self.name
 
 class Products(models.Model):
     name = models.CharField(max_length=150, unique=True, verbose_name='Название')
@@ -24,6 +27,7 @@ class Products(models.Model):
         db_table = 'product'
         verbose_name = 'продукт'
         verbose_name_plural = 'продукты'
-
+    def __str__(self):
+        return f'{self.name} Количество {self.quantity}'
 
 
